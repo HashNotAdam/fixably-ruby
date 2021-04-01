07a9aac7e2d74676c3eb6e6cb24208b34041dd49
Adam Rice <development@hashnotadam.com>
2021-04-01 19:01:12 +1100

    feat: new gem installation

---
+++ spec/fixably_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Fixably do
+  it "has a version number" do
+    expect(Fixably::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
